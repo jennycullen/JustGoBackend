309327260056f362baaf6d47834cab23e5070a22
root <xheni.cullen@gmail.com>
2017-05-12 10:21:34 -0700

    stats.js updated

--- controllers/stats.js
+++
@@ -1,7 +0,0 @@
-const User = require('../models/user');
-const Product = require('../models/product');
-const Order = require('../models/order');
-const Category = require('../models/category');
-
-const roles = ['user', 'admin', 'rider'];
-
